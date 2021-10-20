b76e99d7dc9f33ed1ffeeba591e332ff356959b4
Alexander Prisyazhnyy <sashaprisazhny@gmail.com>
2021-10-20 15:18:37 +0300

    V8 Event Loop Frame Simplified

---
+++ v8-event-loop-frame.js
@@ -0,0 +1,32 @@
+// Constantly iterating.
+// One frame takes ~1/60th of a second.
+
+while (true) {
+  // Taking another SINGLE Task Queue item and execute it.
+  // Task queue contains CALLBACKS of happened EVENTS.
+  const queue = getNextQueue();
+  const taks = queue.pop();
+  execute(task);
+
+  // Executing the WHOLE Microtasks Queue.
+  // Tasks added within the frame are executed too until the queue is EMPTY.
+  // Microtasks Queue mostly consists of PROMISES.
+  while (microtaskQueue.hasTasks()) {
+    exeute(microtask);
+  }
+  
+  // Repaint stage
+  if (isRepaintTime()) {
+    // Executing the WHOLE Animation Queue.
+    // Tasks added within the frame are executed NEXT TIME.
+    // Animation tasks are CALLBACKS of requestAnimationFrame(c: callback)
+    // Useful for executing opposite actions one after another.
+    const animationTasks = animationQueue.copyTasks();
+    
+    for (animationTask in animationTasks) {
+      execute(animationTask);
+    }
+    
+    repaint();
+  }
+}
